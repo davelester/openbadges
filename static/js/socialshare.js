f0cdc6e3782a4317a9985d6bf8440c8b8771f933
Dave Lester <davelester@gmail.com>
2012-11-21 00:38:46 -0800

    Removes fb text

--- static/js/socialshare.js
+++ static/js/socialshare.js
@@ -143,9 +143,9 @@ window.fbAsyncInit = function() {
 	    var accessToken = response.authResponse.accessToken;
 			$('.share-badge').append('<p><a href="#" class="share-badge-on-fb">Share on Facebook</a></p>');
 	  } else if (response.status === 'not_authorized') {
-			$('<div class="fb-login-button">fb</div>').appendTo('.share-badge');
+			$('<div class="fb-login-button"></div>').appendTo('.share-badge');
 	  } else {
-			$('<div class="fb-login-button">fb</div>').appendTo('.share-badge');
+			$('<div class="fb-login-button"></div>').appendTo('.share-badge');
 	  }
 	}, true);
 };
