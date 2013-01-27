612ca446fd199d705ee5815540c589892b134099
Dave Lester <davelester@gmail.com>
2013-01-27 00:08:24 -0800

    Removes unused coffeescript on badge detail page.

--- views/badge-details.hogan.js
+++ views/badge-details.hogan.js
@@ -26,72 +26,16 @@
       {{/type.issuer.org}}
     </dl>
 
+		{{#owner}}
 		<h3>Share these Badges:</h3>
 		<div class="share-badge"><div class="fb-login-button"></div></div>
 
-		{{#owner}}
-		  <form action="{{ deleteRoute }}" method="post" style="display: inline">
-		    <input type="hidden" name="_csrf" value="{{ csrfToken }}"></input>
-		    <input type="hidden" name="_method" value="delete"></input>
-		    <input class="btn btn-danger" type="submit" value="Delete this badge."></input>
-		  </form>
+    <h3>Adminsitration</h3>
+	  <form action="{{ deleteRoute }}" method="post" style="display: inline">
+	    <input type="hidden" name="_csrf" value="{{ csrfToken }}"></input>
+	    <input type="hidden" name="_method" value="delete"></input>
+	    <input class="btn btn-danger" type="submit" value="Delete this badge."></input>
+	  </form>
 	  {{/owner}}
   </div>
 </div>
-
-<script type="text/javascript">
-(function() {
-
-  coffeescript(function() {
-    var autocheck, checkboxes, newGroup, shortDisable, watchChanges;
-    newGroup = $('#new-group');
-    checkboxes = $('.input-append input[type=checkbox]');
-    watchChanges = function(event) {
-      var elem, input, label;
-      elem = $(this);
-      label = elem.parent();
-      input = label.siblings('input').first();
-      if (elem.attr('checked')) {
-        label.addClass('active');
-        if (!input.val()) return input.trigger('focus');
-      } else {
-        return label.removeClass('active');
-      }
-    };
-    autocheck = function(event) {
-      var checkbox, checked, elem;
-      elem = $(this);
-      checkbox = elem.siblings('label').first().find('input');
-      checked = elem.val() ? true : false;
-      return checkbox.attr('checked', checked).trigger('change');
-    };
-    shortDisable = function() {
-      var checkbox, elem;
-      elem = $(this);
-      checkbox = elem.siblings('label').first().find('input');
-      checkbox.attr('disabled', true);
-      return setTimeout(function() {
-        return checkbox.attr('disabled', false);
-      }, 20);
-    };
-    checkboxes.bind('change', watchChanges).trigger('change');
-    return newGroup.bind('keydown', autocheck).bind('blur', autocheck).bind('blur', shortDisable);
-  });
-</script>
-<script>
-(function() {
-
-  coffeescript(function() {
-    var image;
-    image = $('#badge-image');
-    return image.bind('load', function(event) {
-      if (this.clientWidth > 256) {
-        return $(this).css({
-          width: '256px'
-        });
-      }
-    });
-  });
-
-}).call(this);
-</script>
