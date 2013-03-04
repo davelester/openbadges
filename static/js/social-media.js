bbad444f028e4d6e47bf019c3a22d15fe5a14409
Dave Lester <davelester@gmail.com>
2013-03-04 03:56:49 -0800

    Call Facebook SDK and initialize app upon loading.

--- static/js/social-media.js
+++ static/js/social-media.js
@@ -55,3 +55,20 @@ var SocialMedia = function() {};
     }
   };
 }());
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '268806889891263',
+		status     : true,
+		cookie     : true,
+		xfbml      : true 
+	});
+};
+
+// Load the SDK Asynchronously
+(function(d){
+	var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+	js = d.createElement('script'); js.id = id; js.async = true;
+	js.src = "//connect.facebook.net/en_US/all.js";
+	d.getElementsByTagName('head')[0].appendChild(js);
+}(document));
